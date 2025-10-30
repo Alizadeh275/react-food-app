3f349de7a79ad90a2c14b90f85cd7ed4ee95ee0a
= <sajjadalizadeh275800@gmail.com>
2025-10-30 16:26:14 +0330

    added form tag and onSubmit event

--- src/components/Modals/Checkout.jsx
+++ src/components/Modals/Checkout.jsx
@@ -24,8 +24,9 @@ export default function Checkout({ isOpen, onClose, cartItems, total }) {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, [isOpen, onClose]);
 
-  const handleSubmitOrder = () => {
+  const handleSubmitOrder = (event) => {
     // Simulate successful order submission
+    event.preventDefault();
     onClose(); // close checkout modal
     setTimeout(() => setSuccessOpen(true), 100); // open success modal
   };
@@ -34,28 +35,30 @@ export default function Checkout({ isOpen, onClose, cartItems, total }) {
     <>
       {createPortal(
         <dialog ref={dialogRef} className="modal">
-          <h2 className="center">تسویه حساب</h2>
-          <div className="control">
-            <label htmlFor="name">نام و نام خانوادگی</label>
-            <input required id="name" type="text" placeholder="نام شما" />
-          </div>
-          <div className="control">
-            <label htmlFor="email">ایمیل</label>
-            <input required id="email" type="email" placeholder="ایمیل شما" />
-          </div>
-          <div className="control">
-            <label htmlFor="address">آدرس</label>
-            <input required id="address" type="text" placeholder="آدرس شما" />
-          </div>
+          <form onSubmit={handleSubmitOrder}>
+            <h2 className="center">تسویه حساب</h2>
+            <div className="control">
+              <label htmlFor="name">نام و نام خانوادگی</label>
+              <input required id="name" type="text" placeholder="نام شما" />
+            </div>
+            <div className="control">
+              <label htmlFor="email">ایمیل</label>
+              <input required id="email" type="email" placeholder="ایمیل شما" />
+            </div>
+            <div className="control">
+              <label htmlFor="address">آدرس</label>
+              <input required id="address" type="text" placeholder="آدرس شما" />
+            </div>
 
-          <div className="modal-actions">
-            <button className="text-button" onClick={onClose}>
-              بستن
-            </button>
-            <button className="button" onClick={handleSubmitOrder}>
-              ثبت سفارش
-            </button>
-          </div>
+            <div className="modal-actions">
+              <button className="text-button" onClick={onClose}>
+                بستن
+              </button>
+              <button type="submit" className="button">
+                ثبت سفارش
+              </button>
+            </div>
+          </form>
         </dialog>,
         document.body
       )}
