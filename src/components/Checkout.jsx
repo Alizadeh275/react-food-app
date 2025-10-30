4b42609637287cc307328a6086b54c81c4f76e24
= <sajjadalizadeh275800@gmail.com>
2025-10-30 14:14:41 +0330

    edited some styles

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -37,15 +37,15 @@ export default function Checkout({ isOpen, onClose, cartItems, total }) {
           <h2 className="center">تسویه حساب</h2>
           <div className="control">
             <label htmlFor="name">نام و نام خانوادگی</label>
-            <input id="name" type="text" placeholder="نام شما" />
+            <input required id="name" type="text" placeholder="نام شما" />
           </div>
           <div className="control">
             <label htmlFor="email">ایمیل</label>
-            <input id="email" type="email" placeholder="ایمیل شما" />
+            <input required id="email" type="email" placeholder="ایمیل شما" />
           </div>
           <div className="control">
             <label htmlFor="address">آدرس</label>
-            <input id="address" type="text" placeholder="آدرس شما" />
+            <input required id="address" type="text" placeholder="آدرس شما" />
           </div>
 
           <div className="modal-actions">
